1a4b21f149ba2d8ebbfe00efb76de9f7eb542fea
SaJH <wogur981208@gmail.com>
2024-04-25 00:36:05 +0900

    Fix flake

--- config/django/local.py
+++ config/django/local.py
@@ -1,8 +1,8 @@
-from config.django.base import *  # noqa
+from config.django.base import *
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": f"{BASE_DIR}/db.sqlite3",
+        "NAME": f"{BASE_DIR}/db.sqlite3",  # noqa
     }
 }
